2d9ce209099c0664054da31f15d413e17851e862
wonoh <wonoh@zuminternet.com>
2020-09-18 15:54:15 +0900

    step1,2 만들어보기

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,17 @@
 package com.jetbrains.handson.httpapi
 
+import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
 import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.serialization.json
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomerRoutes()
 }
